98fc273548dd6f80ffc4064489f96eb19d615e71
Fredre <hattingh.fredre@gmail.com>
2019-05-29 20:57:41 +0200

    chaGES TO DEPLOY

--- iUpdate/config/index.js
+++ iUpdate/config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/XUpdate/',
+    assetsPublicPath: '/',
     proxyTable: {},
 
     // Various Dev Server settings
